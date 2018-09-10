e08d8a09390829407671fe693fbb3e4a23f933fe
sema <sema@intersvyaz.net>
2018-09-10 15:33:24 +0500

    Merge remote-tracking branch 'origin/master'

--- firebase_subscribe.js
+++ firebase_subscribe.js
@@ -16,9 +16,11 @@ if ('Notification' in window) {
 
     // по клику, запрашиваем у пользователя разрешение на уведомления
     // и подписываем его
-    $('#subscribe').on('click', function () {
-        subscribe();
-    });
+    // $('#subscribe').on('click', function () {
+    //     alert('asd');
+    //     console.log('123');
+    //     subscribe();
+    // });
 }
 
 function subscribe() {
