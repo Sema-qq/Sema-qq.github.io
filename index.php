7d27d0b0b96fc39a6d5539c4d4229dc8da4780c6
Sema-qq <PO-2-06_024@mail.ru>
2018-09-10 13:31:05 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+//Надо вытащить путь (/hdd/records/archive/cam1), id камеры (1), и конечное имя файла (cam1-01-07-2018-04:30:45.mp4) одной регуляркой
+/*
+$string = '/hdd/records/archive/cam1/cam1-01-07-2018-04:30:45.mp4';
+$string = '/HDD/records/archive/cam12/cam12-01-07-2018-04:30:45.mp4';
+
+$result = preg_match('#/(?<path>[0-9a-z/]+/(?<catalog>motion|archive)/cam(?<id>[0-9]+))/(?<file>cam[^/][0-9a-z-:.]+)#i', $string, $matches);
+
+var_dump($matches);
+
+$result = preg_match_all('#/cam([0-9])+[\-]#i', $string, $matches1);
+
+var_dump($matches1);
+
+$result = preg_match_all('#cam[^/][0-9a-z-:.]+#i', $string, $matches2);
+
+var_dump($matches2);*/
+
+?>
+<script type="text/javascript">
+	Notification.requestPermission(function(permission){
+		// переменная permission содержит результат запроса
+		console.log('Результат запроса прав:', permission);
+	});
+
+	var notification = 	new Notification(
+		'Тестовая мессага!', 
+		{
+			body:"Пользователь кликнул на уведомление",
+			dir: 'auto',
+			icon: 'g.ico',
+			tag: 'https://google.com',
+			onclick: 'location = https://google.com'
+		}
+	);
+
+	// notification.onclick = function() {
+	//   location = notification.tag;
+	// }
+</script>
