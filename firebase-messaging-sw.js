2f9e07d4efab10621218172062ccee8210c37580
sema <sema@intersvyaz.net>
2018-09-10 17:47:39 +0500

    10

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.4.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
 firebase.initializeApp({
     messagingSenderId: '799694463735'
