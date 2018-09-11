1cc92ae76578941a1dcc7edcb6b008388e4705e8
sema <sema@intersvyaz.net>
2018-09-11 16:30:55 +0500

    16

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -8,26 +8,4 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// messaging-sw.js
-self.addEventListener('notificationclick', function(event) {
-    const target = event.notification.click_action || '/';
-    event.notification.close();
 
-    // этот код должен проверять список открытых вкладок и переключатся на открытую
-    // вкладку с ссылкой если такая есть, иначе открывает новую вкладку
-    event.waitUntil(clients.matchAll({
-        type: 'window',
-        includeUncontrolled: true
-    }).then(function(clientList) {
-        // clientList почему-то всегда пуст!?
-        for (var i = 0; i < clientList.length; i++) {
-            var client = clientList[i];
-            if (client.url == target && 'focus' in client) {
-                // return client.focus();
-            }
-        }
-
-        // Открываем новое окно
-        return clients.openWindow(target);
-    }));
-});
